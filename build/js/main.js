f7fbb4aaf0b187318242e1d3b87519c1b88838f7
Вини <yvinivitin@gmail.com>
2021-09-02 17:00:53 +0300

    login, and elems state

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,16 @@ const header = document.querySelector('.header');
 const menuToggler = document.querySelector('.header__menu');
 const questionsTogglers = document.querySelectorAll('.questions__item button');
 const filterTogglers = document.querySelectorAll('.catalog-filter__item button');
+const filterToggler = document.querySelector('.catalog-filter__open-filter');
+const filterClose = document.querySelector('.catalog-filter__btn-close');
+const loginBtn = document.querySelector('.header__user-item--login a');
+const popup = document.querySelector('.login');
+const popupEmail = popup.querySelector('#login-email');
+const popupClose = popup.querySelector('.login__btn-close');
+const loginForm = popup.querySelector('#login-form');
+const emailInput = loginForm.querySelector('#login-email');
+const loopers = popup.querySelectorAll('.login__looper');
+const body = document.querySelector('body');
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   slidesPerView: 4,
@@ -58,6 +68,22 @@ function elementsListen(arr, activeClass) {
 
 header.classList.remove('header--menu-opened');
 
+if (filterToggler) {
+  filterToggler.classList.remove('opened');
+
+  filterToggler.addEventListener('click', function() {
+    if (filterToggler.classList.contains('opened')) {
+      filterToggler.classList.remove('opened');
+    } else {
+      filterToggler.classList.add('opened');
+    }
+  });
+
+  filterClose.addEventListener('click', function() {
+    filterToggler.classList.remove('opened');
+  });
+}
+
 menuToggler.addEventListener('click', function() {
   if (header.classList.contains('header--menu-opened')) {
     header.classList.remove('header--menu-opened');
@@ -66,7 +92,46 @@ menuToggler.addEventListener('click', function() {
   }
 });
 
+loginBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  loopers.forEach((item) => {
+    item.addEventListener('focus', () => {
+      popupEmail.focus();
+    });
+  });
+
+  popup.classList.remove('hide');
+  body.style.overflow = 'hidden';
+  popupEmail.focus();
+
+  popupClose.addEventListener('click', function() {
+    popup.classList.add('hide');
+    body.style.overflow = 'scroll';
+  });
+
+  document.addEventListener('click', function(evt) {
+    if (evt.target.classList.contains('login')) {
+      popup.classList.add('hide');
+      body.style.overflow = 'scroll';
+    }
+  });
+
+  document.addEventListener('keydown', function(evt) {
+    if (evt.key === 'Escape') {
+      popup.classList.add('hide');
+      body.style.overflow = 'scroll';
+    }
+  });
+});
+
+loginForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  localStorage.setItem('email', emailInput.value);
+
+  loginForm.reset();
+});
+
 elementsListen(questionsTogglers, 'active');
-// elementsListen(filterTogglers, 'active');
-
-
+elementsListen(filterTogglers, 'active');
